94b99a0725d20786b7eb83f3b735efa0adc29922
vipulkandal <vipulkandal@gmail.com>
2023-06-10 01:13:49 +0530

    Redux setup and Routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+
   useEffect(() => {
     testing();
   }, []);
   const testing = () => {
-    fetchDataFromApi("/movie/popular").then((res) => console.log(res));
+    fetchDataFromApi("/movie/popular").then((res) => {
+      dispatch(getApiConfiguration(res));
+    });
   };
-  return <div className="App">App</div>;
-}
 
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.scss";
+
+const Header = () => {
+  return <div>Header Header</div>;
+};
+
+export default Header;
